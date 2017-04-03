34a907a03de72e497a84b267ed1e61f4cc73b56b
anantvats <anant11cse@gmail.com>
2017-04-03 23:38:37 +0530

    updated for space

--- src/main/java/com/viz/util/ConnectionManager.java
+++ src/main/java/com/viz/util/ConnectionManager.java
@@ -9,6 +9,7 @@ public class ConnectionManager {
 		Facebook facebook = new FacebookFactory().getInstance();
 		facebook.setOAuthAppId("1053480687999273", "2d6fafde8d8229e2260cd9baccffec71");
 		facebook.setOAuthPermissions("user_profile");
+		
 		facebook.setOAuthAccessToken(new AccessToken("EAAOZBIpYvmSkBAPIVNeatAvvinqcfy1j6Unz3CB9B9SIdsqNbZB2XphlT7ZB0RJubg8mLSFqG0tPI3AvHQ9B5qqZAQRzOXhfP6IVluYC5stdQ5Q3Q7AspzMUjx0jeuH2e1QFq1lysG0FZCaEFViEICLbH5FzK5kkePPCuNa3YmhyZA1pelq4aTcZAJ7hLC8jCQZD", null));
 		return facebook;
 		
